562fa3d0b81aa0adc72796aa6c171f3f0857c508
ImashaNawodi <imashanaw1999@gmail.com>
2025-07-24 23:43:53 +0530

    color updated

--- blissme-app/src/app/layouts/MainLayout.tsx
+++ blissme-app/src/app/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import bgImage from "../../assets/images/tile.jpg";
 
 const { Sider, Content } = Layout;
 
@@ -9,14 +10,18 @@ const MainLayout = () => {
     <Layout className="w-full flex flex-row h-screen">
       <Sider
         width={250}
-        className={
-          "visible sider !bg-g-gray-900 !max-w-[230px] !min-w-[230px] !w-[230px]"
-        }
+          className="visible sider !bg-green-900 !max-w-[230px] !min-w-[230px] !w-[230px]"
+
       >
         <Sidebar />
       </Sider>
 
-      <Layout className="flex-1 h-screen">
+      <Layout
+        className="w-full flex flex-row h-screen bg-cover bg-center"
+        /* style={{
+          backgroundImage: `url(${bgImage})`,
+        }} */
+      >
         <Content className="w-full h-full p-2 overflow-scroll">
           <Outlet />
         </Content>

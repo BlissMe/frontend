a49924523c5289c1954b5a62eebc2f5953b6849b
ImashaNawodi <imashanaw1999@gmail.com>
2025-12-13 21:12:56 +0530

    testing

--- blissme-app/src/app/chatBox/Chat.tsx
+++ blissme-app/src/app/chatBox/Chat.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ChatInterface from "../../components/chat/ChatInterface";
 import Sidebar from "../../components/chat/Sidebar";
 import bg from "../../assets/images/cb1.png";

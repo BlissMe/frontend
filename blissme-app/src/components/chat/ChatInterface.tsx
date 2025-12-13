4418cd25e85d0419cdfacb4e068028852dbc3bce
Piumi Kavindya <piyumikavindyappk@gmail.com>
2025-12-13 11:52:23 +0530

    commit

--- blissme-app/src/components/chat/ChatInterface.tsx
+++ blissme-app/src/components/chat/ChatInterface.tsx
@@ -246,8 +246,7 @@ const ChatInterface = () => {
   }, []);
 
   const sendEmergencySMS = async () => {
-    const phone =
-      "OGZiMTYwOWU4ZjMzNTk0NDBhN2E1NTg1NTY1Y2IxMjViZTA1NGQ1OGY1ZTk3NmFlNzRhODExZTQ0NWMzNDdiZTptb2JpdGVs";
+    const phone = "94701362783";
     const text =
       "Your session indicates high distress. Please seek help immediately.";
 
